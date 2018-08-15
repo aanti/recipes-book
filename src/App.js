08e77533b6e00342e932705985fc02eefac52997
Magda Chmielewska <magda.chmielewska@comarch.com>
2018-08-15 13:31:12 +0200

    Roboto font + basic body style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import style from './App.scss'
 
 class App extends Component {
   render() {
     return (
       <div className={style.App}>
-
+        recipe
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+
+if (module.hot) {
+  module.hot.accept()
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
