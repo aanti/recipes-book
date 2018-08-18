2d160c2978a3e0d4c6c0caed6028e029d6a4d97c
Magda Chmielewska <magda.chmielewska@comarch.com>
2018-08-18 15:41:37 +0200

    Separation of Collapsed logic

---
+++ src/components/Collapsed/Collapsed.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+
+import style from './Collapsed.scss'
+
+class Collapsed extends Component {
+  static Header = ({ children, className, onClick, headerRef }) => (
+    <div className={className} ref={headerRef} onClick={onClick}>{children}</div>
+  )
+  static Content = ({ open, className, children, contentRef }) => open
+    ? <div className={className} ref={contentRef}>{children}</div>
+    : null
+
+  state = {
+    open: false
+  }
+
+  innerRef = React.createRef() // ref (in order to measure height of content container)
+  outerRef = React.createRef() // ref (in order to measure height of content container)
+  height = 0 // Very initial value of height (modified in componentDidMound lifecycle method)
+  headerHeight = 0 // Very initial value of header height (modified in componentDidMound lifecycle method)
+  initial = true // flag indicating initial render
+
+  handleClick = () => {
+    this.setState(({ open }) => ({ open: !open }))
+  }
+
+  getContainerHeight = () => {
+    const { open } = this.state
+    return open ? this.height : this.headerHeight
+  }
+
+  componentDidMount = () => {
+    this.headerHeight = this.outerRef.current.offsetHeight
+    this.height = this.innerRef.current.clientHeight + this.headerHeight
+    this.initial = false
+    this.forceUpdate() // force update in order to hide expanded content (after measurement of initial render height)
+  }
+
+  render () {
+    const { className, children, onClick } = this.props
+    const { open } = this.state
+    return (
+      <div className={classnames(style.container, className)} style={{ height: this.getContainerHeight() }}>
+        {
+          React.Children.map(children, child => React.cloneElement(child, {
+            open: open || this.initial,
+            onClick: this.handleClick,
+            contentRef: this.innerRef,
+            headerRef: this.outerRef
+          }))
+        }
+      </div>
+    )
+  }
+}
+
+export default Collapsed
--- src/views/RecipeList/RecipeItem/RecipeItem.js
+++ src/views/RecipeList/RecipeItem/RecipeItem.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react'
-import classnames from 'classnames'
 
 import Section from '../../../components/Section/Section'
+import Collapsed from '../../../components/Collapsed/Collapsed'
 
 import style from './RecipeItem.scss'
 
-class RecipeItemContent extends Component {
-  innerRef = React.createRef()
-  height = 0
-  initial = true
-
-  componentDidMount = () => {
-    this.height = this.innerRef.current.clientHeight
-    this.initial = false
-    this.forceUpdate()
-  }
-
+class RecipeItem extends Component {
   handleDelete = (e) => {
     const { id, onDelete } = this.props
     e.stopPropagation()
@@ -29,61 +19,38 @@ class RecipeItemContent extends Component {
   }
 
   render () {
-    const { name, id, description, ingredients = [{}], recipe = [], open, onClick, onDelete, onModify } = this.props
+    const { name, id, description, ingredients, recipe, open, onClick, onDelete, onModify } = this.props
     return (
-      <div
-        className={classnames(style.container, open && style.expanded)}
-        onClick={onClick}
-        style={{ height: open && this.height + 50 }}
-      >
-        <div className={style.name}>{name}</div>
-        <div className={style.description}>{description}</div>
-        <div ref={this.innerRef} className={style.content}>
-          {
-            (this.initial || open) && (
-              <React.Fragment>
-                <Section title="ingredients" titleColor="#fff">
-                  {
-                    ingredients.map(({ product, amount }, i) => (
-                      <div key={i} className={style.ingredient}>
-                        <span>{product}</span>
-                        <span>{amount}</span>
-                      </div>
-                    ))
-                  }
-                </Section>
-                <Section title="recipe" titleColor="#fff">
-                  {
-                    recipe.map((step, i) => (
-                      <div key={i}>
-                        <span>{step}</span>
-                      </div>
-                    ))
-                  }
-                </Section>
-                <div onClick={this.handleModify}>MODIFY RECIPE!</div>
-                <div onClick={this.handleDelete}>DELETE RECIPE!</div>
-              </React.Fragment>
-            )
-          }
-        </div>
-      </div>
+      <Collapsed className={style.container}>
+        <Collapsed.Header className={style.header}>
+          <div className={style.name}>{name}</div>
+          <div className={style.description}>{description}</div>
+        </Collapsed.Header>
+        <Collapsed.Content className={style.content}>
+          <Section title="ingredients" titleColor="#fff">
+            {
+              ingredients.map(({ product, amount }, i) => (
+                <div key={i} className={style.ingredient}>
+                  <span>{product}</span>
+                  <span>{amount}</span>
+                </div>
+              ))
+            }
+          </Section>
+          <Section title="recipe" titleColor="#fff">
+            {
+              recipe.map((step, i) => (
+                <div key={i}>
+                  <span>{step}</span>
+                </div>
+              ))
+            }
+          </Section>
+          <div onClick={this.handleModify}>MODIFY RECIPE!</div>
+          <div onClick={this.handleDelete}>DELETE RECIPE!</div>
+        </Collapsed.Content>
+      </Collapsed>
     )
-  }
-}
-
-class RecipeItem extends Component {
-  state = {
-    open: false
-  }
-
-  handleClick = () => {
-    this.setState(({ open }) => ({ open: !open }))
-  }
-
-  render () {
-    const { open } = this.state
-    return <RecipeItemContent {...this.props} open={open} onClick={this.handleClick} />
   }
 }
 
