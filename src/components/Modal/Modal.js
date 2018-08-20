cd4591c1fddbf64dc3b4404f7e293b69072a834e
Magda Chmielewska <magda.chmielewska@comarch.com>
2018-08-20 09:51:31 +0200

    React helmet in order to hide

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
 import classnames from 'classnames'
 
 import style from './Modal.scss'
@@ -14,7 +15,9 @@ const Modal = ({ children, className, open }) => (
               <div className={classnames(style.modal, className)}>
                 {children}
               </div>
+              <Helmet bodyAttributes={{ style: 'overflow: hidden' }}/>
             </div>
+
           </Fragment>
         :
           null
